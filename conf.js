3bb006262ec83109da529021aff02d244a41030b
lukeclever <60529895+LUKECLEVER@users.noreply.github.com>
2021-08-22 13:20:44 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '61990',
+  ver: '61999',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -99,8 +99,8 @@ jsproxy_config({
         //'b.hehe.workers.dev': 1,
         //'b.lulu.workers.dev': 1,
         //'b.jsproxy.workers.dev': 1,
-        'proxy.luyao.workers.dev': 1,
-        'proxy.lukeclever.workers.dev': 1,
+        'proxy.luyao.workers.dev': 7,
+        'proxy.lukeclever.workers.dev': 7,
       }
     }
   },
